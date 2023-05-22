a527549a58d3d3400dfb3b20352f1a73a79c3f7e
Kt-Mm <ktine.celmlo@gmail.com>
2023-05-22 16:21:26 -0400

    first commit

---
+++ veryhard.js
@@ -0,0 +1,27 @@
+//VERY HARD: You are given coins of different denominations 
+//and a total amount of money amount. Write a function to 
+//compute the fewest number of coins that you need to make up 
+//that amount. If that amount of money cannot be made up by any 
+//combination of the coins, return -1.
+//Hint:  Sudo code this problem. Focus on breaking the problem 
+//down into steps  Use functions, arrays and logical operators.  
+//Do not have anyone give you the answer or solve this problem for you. 
+
+//1-create function
+//2-create variable = array
+//3- create a IF logical operator.
+//4-amount = 11, coins [1, 2, 5 ]
+
+function coinsNeed() {
+    
+    let arr = [1 + 2 * 5];
+    let x = "";
+    if (arr == 11 ) {
+        x = arr.lenght
+    }else{
+        -1
+    }
+    return x;
+}
+console.log(coinsNeed);
+
